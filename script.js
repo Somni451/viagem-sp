8e062aa5aa907b1cf7c5f9e858584ebb7bcd3955
Somni451 <92165916+Somni451@users.noreply.github.com>
2026-09-16 18:00:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const SUPABASE_URL = 'https://svlllemeaoglofgkjwqy.supabase.co';
-const SUPABASE_KEY = 'COLOQUE_SUA_PUBLISHABLE_KEY_AQUI';
 
-const { createClient } = supabase;
+const SUPABASE_KEY = 'sb_publishable_awG6lCJsMGP7n35B2yV20w_h46ATS9L';
 
-const db = createClient(SUPABASE_URL, SUPABASE_KEY);
+const db = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
 
-async function testConnection() {
+async function loadAttractions() {
   const { data, error } = await db
     .from('attractions')
-    .select('*');
+    .select('*')
+    .order('created_at', { ascending: true });
 
   if (error) {
-    console.error('Erro ao conectar:', error);
+    console.error('Erro ao carregar atrações:', error);
     return;
   }
 
-  console.log('Atrações encontradas:', data);
+  console.log('Atrações carregadas:', data);
 }
 
-testConnection();
+loadAttractions();
